cef8ee96ae4aa690b6789ed95d28260b8070bacd
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-11-13 00:49:18 -0800

    okur

---
+++ src/Components/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Button.less';
+
+const CustomButton = (props) => {
+    const style = {
+        'background': props.color,
+        'font-size': props.size,
+        'padding': `8px 0` ,
+        'width': props.width,
+        'color': 'white !important' 
+    }
+
+    return (
+        <Button
+        className = "button"
+        type="primary"
+        onClick={props.onClick}
+        style = {style}
+        htmlType={props.htmlType}>
+            {props.children}
+        </Button>
+    );
+};
+
+export default CustomButton;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Home from './Home.css';
+import { NavLink } from 'react-router-dom';
+import Button from './Button.jsx';
 
-const Overview = () => {
-  return <h1>this is the Overview tab</h1>
-}
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="content">
+        <h1> Helping Hands </h1>
+        <div className="subcontent">
+          <h2> An organization dedicated to help you help others.  </h2>
+          <p> An application to connect you to organizations, communities, and charities. </p>
+        </div>
+        <NavLink activeClassName="selected" to="/ibegin">
+          <Button color='#B6CBFE' size='25px' width='250px'>
+            Volunteer
+          </Button>
+        </NavLink>
 
-export default Overview;
+        <NavLink activeClassName="selected" to="/">
+          <Button color='#B6CBFE' size='25px' width='250px'>
+            Organization
+          </Button>
+        </NavLink>
+        <div className="floatingPerson">
+          <img src={floating} alt="floating"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
